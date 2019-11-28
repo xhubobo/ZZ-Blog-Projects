62e533a75feb68a18327a35dcc0d1d8158ff9042
xhubobo <19bobo@sina.com>
2019-11-29 00:25:57 +0800

    + [设计模式] 添加单例模式

---
+++ Design Pattern/SingletonPattern/SingletonPattern/Simple/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonPattern.Simple
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
