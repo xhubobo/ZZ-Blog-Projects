df24ac816451741e4f9fce047069008961a105a4
zhangbo <19bobo@sina.com>
2020-03-20 15:45:23 +0800

    添加控制现有PPT的方法

---
+++ Office/PptSample/PptSample/ControlForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PptSample
+{
+    partial class ControlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonDetect = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 260);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonDetect
+            // 
+            this.buttonDetect.Location = new System.Drawing.Point(13, 13);
+            this.buttonDetect.Name = "buttonDetect";
+            this.buttonDetect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDetect.TabIndex = 1;
+            this.buttonDetect.Text = "Detect";
+            this.buttonDetect.UseVisualStyleBackColor = true;
+            this.buttonDetect.Click += new System.EventHandler(this.buttonDetect_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(116, 13);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(214, 13);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrev.TabIndex = 1;
+            this.buttonPrev.Text = "Prev";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(306, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 382);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonDetect);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ControlForm";
+            this.Text = "ControlForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonDetect;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Office/PptSample/PptSample/ControlForm.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Application = Microsoft.Office.Interop.PowerPoint.Application;
+
+namespace PptSample
+{
+    public partial class ControlForm : Form
+    {
+        private Application _app; //PPT进程
+        private readonly List<SlideShowWindow> _slideShowWindows = new List<SlideShowWindow>();
+
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDetect_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            _slideShowWindows.Clear();
+            try
+            {
+                _app = Marshal.GetActiveObject("PowerPoint.Application") as Application;
+            }
+            catch (Exception exception)
+            {
+                textBox1.Text = "没有打开PPT";
+                return;
+            }
+
+            if (_app == null)
+            {
+                textBox1.Text = "没有打开PPT";
+                return;
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"共有{_app.Presentations.Count}个打开的PPT：");
+            foreach (Presentation presentation in _app.Presentations)
+            {
+                sb.AppendLine($"{presentation.Path}\\{presentation.Name}");
+                sb.AppendLine($"{presentation.Slides.Count}页");
+                AddSlideShowWindow(presentation);
+            }
+
+            textBox1.Text = sb.ToString();
+        }
+
+        private void AddSlideShowWindow(Presentation presentation)
+        {
+            var slideShowSettings = presentation?.SlideShowSettings;
+            if (slideShowSettings == null)
+            {
+                return;
+            }
+
+            //窗口设置
+            //ppShowTypeSpeaker 1   演讲者
+            //ppShowTypeWindow  2   窗口
+            //ppShowTypeKiosk   3   展台
+            slideShowSettings.ShowType = PpSlideShowType.ppShowTypeWindow;
+            //ppSlideShowManualAdvance      手动换页
+            //ppSlideShowUseSlideTimings    为每张幻灯片指定的计时
+            //ppSlideShowRehearseNewTimings 排练计时
+            //slideShowSettings.AdvanceMode = PpSlideShowAdvanceMode.ppSlideShowManualAdvance;
+            slideShowSettings.ShowScrollbar = MsoTriState.msoFalse;
+            slideShowSettings.ShowPresenterView = MsoTriState.msoFalse; //返回SlideShowSettings对象的演示者视图
+            slideShowSettings.ShowMediaControls = MsoTriState.msoFalse; //允许访问SlideShowSettings对象中的媒体控件
+            slideShowSettings.ShowWithNarration = MsoTriState.msoFalse; //确定显示指定幻灯片放映时是否有旁白
+            //是否显示页内动画
+            slideShowSettings.ShowWithAnimation = MsoTriState.msoTrue;
+
+            var window = slideShowSettings.Run();
+            _slideShowWindows.Add(window);
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            var presentations = _app?.Presentations;
+            if (presentations == null)
+            {
+                return;
+            }
+
+            try
+            {
+                foreach (var window in _slideShowWindows)
+                {
+                    window.View.Next();
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            var presentations = _app?.Presentations;
+            if (presentations == null)
+            {
+                return;
+            }
+
+            try
+            {
+                foreach (var window in _slideShowWindows)
+                {
+                    window.View.Previous();
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
