bcb697d316ee3c2c7b2da63150d66b485bf6fee8
xhubobo <19bobo@sina.com>
2020-11-28 10:43:23 +0800

    * [NewsPublish] 添加新闻类别管理

---
+++ NetCore/NewsPublish/NewsPublish.Web/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using NewsPublish.Model.Request;
+using NewsPublish.Model.Response;
+using NewsPublish.Service;
+
+namespace NewsPublish.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NewsController : Controller
+    {
+        private readonly NewsService _newsService;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public NewsController(NewsService newsService, IHostingEnvironment hostingEnvironment)
+        {
+            _newsService = newsService;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: NewsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NewsController/NewsClassify
+        public ActionResult NewsClassify()
+        {
+            var newsClassifyList = _newsService.GetNewsClassifyList();
+            return View(newsClassifyList);
+        }
+
+        // GET: NewsController/AddNewsClassify
+        public ActionResult AddNewsClassify()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddNewsClassify(AddNewsClassify addNewsClassify)
+        {
+            return Json(string.IsNullOrEmpty(addNewsClassify.Name)
+                ? new ResponseModel(0, "请输入新闻类别名称")
+                : _newsService.AddNewsClassify(addNewsClassify));
+        }
+
+        // GET: NewsController/EditNewsClassify
+        public ActionResult EditNewsClassify(int newsClassifyId)
+        {
+            return View(_newsService.GetNewsClassify(newsClassifyId));
+        }
+
+        [HttpPost]
+        public JsonResult EditNewsClassify(EditNewsClassify editNewsClassify)
+        {
+            return string.IsNullOrEmpty(editNewsClassify.Name)
+                ? Json(new ResponseModel(0, "请输入新闻类别名称"))
+                : Json(_newsService.EditNewsClassify(editNewsClassify));
+        }
+    }
+}
