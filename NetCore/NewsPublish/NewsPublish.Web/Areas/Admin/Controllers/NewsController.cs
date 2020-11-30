9a28c5f29a5c90355bb9230b80b4704e2812c57e
xhubobo <19bobo@sina.com>
2020-11-30 13:50:38 +0800

    * [NewsPublish] 添加“添加/删除”新闻功能

--- NetCore/NewsPublish/NewsPublish.Web/Areas/Admin/Controllers/NewsController.cs
+++ NetCore/NewsPublish/NewsPublish.Web/Areas/Admin/Controllers/NewsController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using NewsPublish.Model.Entity;
 using NewsPublish.Model.Request;
 using NewsPublish.Model.Response;
@@ -21,6 +25,8 @@ namespace NewsPublish.Web.Areas.Admin.Controllers
             _newsService = newsService;
             _hostingEnvironment = hostingEnvironment;
         }
+
+        #region 新闻
 
         // GET: NewsController
         public ActionResult Index()
@@ -46,6 +52,62 @@ namespace NewsPublish.Web.Areas.Admin.Controllers
             var news = _newsService.QueryNewsPage(pageSize, pageIndex, out var total, whereList);
             return Json(new {total = total, data = news.Data});
         }
+
+        public ActionResult AddNews()
+        {
+            var newsClassifyList = _newsService.GetNewsClassifyList();
+            return View(newsClassifyList);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddNews(AddNews addNews, IFormCollection formCollection)
+        {
+            if (addNews.NewsClassifyId <= 0 || string.IsNullOrWhiteSpace(addNews.Title) ||
+                string.IsNullOrWhiteSpace(addNews.Contents))
+            {
+                return Json(new ResponseModel(0, "参数有误"));
+            }
+
+            var files = formCollection.Files;
+            if (files.Count == 0)
+            {
+                return Json(new ResponseModel(0, "请上传图片文件"));
+            }
+
+            var webRootPath = _hostingEnvironment.WebRootPath;
+            var absoluteDirPath = Path.Combine(webRootPath, "NewsPic");
+            var fileTypes = new[] {".gif", ".jpg", ".jpeg", ".png", ".bmp"};
+            var extension = Path.GetExtension(files[0].FileName);
+            if (!fileTypes.Contains(extension))
+            {
+                return Json(new ResponseModel(0, "图片格式有误"));
+            }
+
+            if (!Directory.Exists(absoluteDirPath))
+            {
+                Directory.CreateDirectory(absoluteDirPath);
+            }
+
+            var fileName = $"{DateTime.Now:yyyyMMddHHmmss}{extension}";
+            var filePath = Path.Combine(absoluteDirPath, fileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await files[0].CopyToAsync(stream);
+            }
+
+            addNews.Image = $"/NewsPic/{fileName}";
+            return Json(_newsService.AddNews(addNews));
+        }
+
+        [HttpPost]
+        public JsonResult DeleteNews(int newsId)
+        {
+            return Json(newsId <= 0
+                ? new ResponseModel(0, "新闻不存在")
+                : _newsService.DeleteNews(newsId));
+        }
+
+        #endregion
 
         #region 新闻类别
 
