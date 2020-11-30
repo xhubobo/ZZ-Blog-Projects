804e2226ee2f797038b16df2b3b0815d5c50ef9c
xhubobo <19bobo@sina.com>
2020-11-30 14:33:12 +0800

    * [NewsPublish] 添加后台评论管理

---
+++ NetCore/NewsPublish/NewsPublish.Web/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Model.Response;
+using NewsPublish.Service;
+
+namespace NewsPublish.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        private readonly CommentService _commentService;
+
+        public CommentController(CommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_commentService.GetCommentList(t => true));
+        }
+
+        [HttpPost]
+        public JsonResult DeleteComment(int commentId)
+        {
+            return Json(commentId <= 0 
+                ? new ResponseModel(0, "参数有误") 
+                : _commentService.DeleteComment(commentId));
+        }
+    }
+}
