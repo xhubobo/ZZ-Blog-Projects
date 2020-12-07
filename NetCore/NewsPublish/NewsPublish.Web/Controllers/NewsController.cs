2820b42d9c8b8348ee1f402ee1ef58899c094355
xhubobo <19bobo@sina.com>
2020-12-07 10:08:38 +0800

    * [NewsPublish] 添加新闻详情页

--- NetCore/NewsPublish/NewsPublish.Web/Controllers/NewsController.cs
+++ NetCore/NewsPublish/NewsPublish.Web/Controllers/NewsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Model.Response;
 using NewsPublish.Service;
 
 namespace NewsPublish.Web.Controllers
@@ -6,10 +7,12 @@ namespace NewsPublish.Web.Controllers
     public class NewsController : Controller
     {
         private readonly NewsService _newsService;
+        private readonly CommentService _commentService;
 
-        public NewsController(NewsService newsService)
+        public NewsController(NewsService newsService, CommentService commentService)
         {
             _newsService = newsService;
+            _commentService = commentService;
         }
 
         public ActionResult Classify(int classifyId)
@@ -34,6 +37,38 @@ namespace NewsPublish.Web.Controllers
 
                 var latestCommentNewsList = _newsService.GetLatestNewsListByComment(t => t.NewsClassifyId == classifyId, 6);
                 ViewData["LatestCommentNewsList"] = latestCommentNewsList;
+            }
+
+            return View(_newsService.GetNewsClassifyList());
+        }
+
+        public IActionResult Detail(int newsId)
+        {
+            ViewData["Title"] = "详情页";
+            ViewData["News"] = new ResponseModel();
+            ViewData["RecommendNewsList"] = new ResponseModel();
+            ViewData["CommentList"] = new ResponseModel();
+
+            if (newsId < 0)
+            {
+                Response.Redirect("/Home/Index");
+            }
+
+            var news = _newsService.GetNews(newsId);
+            if (news.Code == 0)
+            {
+                Response.Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewData["Title"] = news.Data.Title + "-" + ViewData["Title"];
+                ViewData["News"] = news;
+
+                var recommendNewsList = _newsService.GetRecommendNewsList(newsId);
+                ViewData["RecommendNewsList"] = recommendNewsList;
+
+                var commentList = _commentService.GetCommentList(t => t.NewsId == newsId);
+                ViewData["CommentList"] = commentList;
             }
 
             return View(_newsService.GetNewsClassifyList());
